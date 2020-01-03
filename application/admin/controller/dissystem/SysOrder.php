3488a01c2a0d6ff814d2fc8f7ac76ead830f09d7
fangfang0917 <1553759177@qq.com>
2020-01-03 15:20:48 +0800

    update

--- application/admin/controller/dissystem/SysOrder.php
+++ application/admin/controller/dissystem/SysOrder.php
@@ -1,5 +1,5 @@
 <?php
-namespace app\admin\controller\shop;
+namespace app\admin\controller\dissystem;
 
 \think\Loader::import('controller/Controller', \think\Config::get('traits_path') , EXT);
 
@@ -23,6 +23,54 @@ class SysOrder extends Controller
         }
         if ($this->request->param("type")) {
             $map['type'] = ["like", "%" . $this->request->param("type") . "%"];
+        }
+    }
+    public function index()
+    {
+
+        $model = $this->getModel('SysOrder');
+        // 列表过滤器，生成查询Map对象
+        $map = $this->search($model, [$this->fieldIsDelete => $this::$isdelete]);
+
+        // 特殊过滤器，后缀是方法名的
+        $actionFilter = 'filter' . $this->request->action();
+
+        if (method_exists($this, $actionFilter)) {
+            $this->$actionFilter($map);
+        }
+
+        // 自定义过滤器
+        if (method_exists($this, 'filter')) {
+            $this->filter($map);
+        }
+
+
+        $this->datalist($model, $map);
+
+        return $this->view->fetch();
+    }
+
+    public function edit()
+    {
+        if($this ->request->isAjax()){
+            $data = $this->request->except(['id']);
+            $id = $this->request->param('id');
+            if($data['type'] == 4){
+                if($data['remarks'] == ''){
+                    return ajax_return_adv_error('请填写备注');
+                }
+            }
+            $type = Db('SysOrder')->where(array('id'=>$id))->value('type');
+            if($data['type'] <= $type){
+                return ajax_return_adv_error('状无法回溯');
+            }
+            Db('SysOrder')->where(array('id'=>$id))->data($data)->update();
+            return ajax_return_adv('提交成功!!!!');
+        }else{
+            $id = $this ->request->param('id');
+            $vo = Db('SysOrder')->where(array('id'=>$id))->find();
+            $this->view->assign('vo',$vo);
+            return $this ->view->fetch();
         }
     }
 }
